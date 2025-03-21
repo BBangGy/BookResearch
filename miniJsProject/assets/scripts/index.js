fe0f3bdd900192aceea3e30a4d419aecbf544b95
정현규 <hyeongyu-98@naver.com>
2025-03-21 17:45:58 +0900

    도서관 표시
    현재위치에 도서관 표시 여러개하기 성공 다음에는 입력 받은 걸 기반으로 위치 표시하기

--- miniJsProject/assets/scripts/index.js
+++ miniJsProject/assets/scripts/index.js
@@ -9,11 +9,12 @@ const $bestBookItems = $bestSearch.querySelector(':scope > .show-ranks > .list')
 const $contentBox = $searchDialog.querySelector(':scope > .layout-content > .popular-container > .content-box');
 
 const apiKey = '8Lk0MkLHo573DCbWLuJXqkh3aCtUK5vbm1NPP%2B13GqQzF9DXlFtKmZEy0iVPtqoWQHnKh6j6gsaEpDTdrXZJVg%3D%3D';
-
+const restApiKey = 'fa8fbe051d67927bcc168a007e37a73b';
 const hideLoading = () => $loading.classList.remove('visible');
 const showLoading = () => $loading.classList.add('visible');
 const hideDialog = () => $findDialog.classList.remove('visible');
 const showDialog = () => $findDialog.classList.add('visible');
+
 
 $findDialog.querySelector(':scope > .close').onclick = () => {
     hideDialog()
@@ -58,13 +59,12 @@ function fetchBookData(bookTitle, sortType) {
         const books = response.documents;
         const totalCount = response.meta.total_count;
         console.log(response);
-
         renderBooks(books, totalCount);
         showDialog();
     };
 
     xhr.open('GET', url.toString());
-    xhr.setRequestHeader('Authorization', 'KakaoAK fa8fbe051d67927bcc168a007e37a73b');
+    xhr.setRequestHeader('Authorization', `KakaoAK ${restApiKey}`);
     xhr.send();
     showLoading();
 }
@@ -126,6 +126,7 @@ function rankList() {
         const response = JSON.parse(xhr.responseText);
         console.log(response)
         const datas = response.response.body.items.item;
+        console.log(datas)
         let rankTitle = [];
 
         $contentBox.innerHTML = "";
@@ -161,7 +162,7 @@ function rankList() {
             getSearchImg(item.title, item.img);
         }
     };
-    const url = `https://apis.data.go.kr/6260000/BookSearchWordBestService/getBookSearchWordBest?serviceKey=${apiKey}&numOfRows=10&pageNo=1&resultType=json`;
+    const url = `https://apis.data.go.kr/6260000/BookSearchWordBestService/getBookSearchWordBest?serviceKey=${apiKey}&numOfRows=10&pageNo=1&lib_name=0&resultType=json`;
     xhr.open('GET', url);
     xhr.send();
 }
@@ -185,7 +186,7 @@ function getSearchImg(rankTitle, img) {
     };
 
     xhr.open("GET", url);
-    xhr.setRequestHeader("Authorization", "KakaoAK fa8fbe051d67927bcc168a007e37a73b");
+    xhr.setRequestHeader("Authorization", `KakaoAK ${restApiKey}`);
     xhr.send();
 }
 
@@ -199,6 +200,7 @@ document.addEventListener("click", (e) => {
     const title = titleElement.innerText.trim();
     $find.value = title;
     console.log(title);
+
 
     fetchBookData(title);
 });
@@ -214,11 +216,112 @@ const mapInstance = new kakao.maps.Map($map,
         center: new kakao.maps.LatLng(35.8655753, 128.59339),
         level: 3,
     }
+    //처음에 지도를 열었을때 표시해주는 위도와경도를 정해서 띄워준다.
 );
+const input = $mapContainer.querySelector(':scope>.location-search>.label>.book-title');
+const nearBy = input.value;
 document.getElementById('confirm').onclick = () => {
     showMapDialog();
+    currentLocation();
 }
 
 $close.onclick = () => {
     hideMapDialog();
 }
+
+let destinationMarker;
+let currentMarker;
+
+const currentInfoWindow = new kakao.maps.InfoWindow({
+    content: '<div style="width: 150px; text-align: center; padding: 0.5rem 1rem;background-color:deepskyblue; color: #ffffff;">현위치</div>'
+})
+const destinationInfoWindow = new kakao.maps.InfoWindow({
+    content: '<div style="width:150px; text-align: center; padding:0.5rem 1rem;">도착지</div>',
+})
+function currentLocation() {
+    //navigator.geolocation를 사용해 현재 위치를 작성할 수 있다.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            let lat = position.coords.latitude;//현재 위치의 위도
+            let lng = position.coords.longitude;//현재 위치의 경도
+            let locPosition = new kakao.maps.LatLng(lat, lng);
+            //현재 위치의 위도와 경도를 지정.
+
+            currentMarker = new kakao.maps.Marker({
+                map: mapInstance,
+                position: locPosition,
+            })
+
+            currentInfoWindow.open(mapInstance, currentMarker);
+
+        })
+    }
+}
+
+$mapContainer.querySelector(':scope>.location-search>.find').onclick = (e) => {
+    e.preventDefault();
+    console.log(findLocation('대구도서관'));
+}
+
+let coordinates;
+
+function markerOverListener(map, marker, infowindow) {
+    return function () {
+        infowindow.open(map, marker)
+    }
+}
+function markerOutListener(infowindow){
+    return () => {
+        infowindow.close();
+    };
+}
+function findLocation(destination) {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== XMLHttpRequest.DONE) {
+            return;
+        }
+        hideLoading();
+        if (xhr.status < 200 || xhr.status >= 400) {
+            alert('요청실패');
+            return;
+        }
+        const response = JSON.parse(xhr.responseText);
+        console.log(response);
+        //주변 도서관 위치를 배열로 저장하기 위한 변수
+        const libraryList = [];
+        for (let i = 0; i < response.documents.length; i++) {
+            coordinates = new kakao.maps.LatLng(response.documents[i].y, response.documents[i].x);
+            libraryList.push(coordinates);
+        }
+        //도서관 위치에다가 마크 표시하기 성공
+        for (let i = 0; i < libraryList.length; i++) {
+            destinationMarker = new kakao.maps.Marker({
+                map: mapInstance,
+                position: libraryList[i],
+            })
+            let infoWindow = new kakao.maps.InfoWindow({
+                content:`도서관${[i]}`
+            })
+            kakao.maps.event.addListener(destinationMarker, 'mouseover', markerOverListener($map, destinationMarker, infoWindow));
+            kakao.maps.event.addListener(destinationMarker, 'mouseout', markerOutListener(infoWindow));
+        }
+        // coordinates = new kakao.maps.LatLng(response.documents[0].y, response.documents[0].x);
+        // destinationMarker = new kakao.maps.Marker({
+        //     map: mapInstance,
+        //     position: coordinates,
+        // })
+        // destinationInfoWindow.open(mapInstance, destinationMarker);
+        // console.log(coordinates);
+    }
+
+    const url = new URL('https://dapi.kakao.com/v2/local/search/keyword');
+    url.searchParams.set('query', `${destination}`);
+    url.searchParams.set('radius', '10000'); //반경 10km안에서 알려준다.
+    // url.searchParams.set('category_group_code', 'CT1');
+    xhr.open('GET', url.toString());
+    xhr.setRequestHeader('Authorization', `KakaoAK ${restApiKey}`);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send();
+}
+
